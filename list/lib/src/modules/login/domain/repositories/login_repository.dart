e4c7fea45c279bd495a0878f72ea455c3f8dca34
joaoffnogueira <contact@joaonogueira.dev>
2023-12-17 10:05:35 -0300

    add save user

--- list/lib/src/modules/login/domain/repositories/login_repository.dart
+++ list/lib/src/modules/login/domain/repositories/login_repository.dart
@@ -2,8 +2,7 @@ import 'package:dartz/dartz.dart';
 import '../entities/login_entity.dart';
 
 abstract class LoginRepository {
-  Future<Either<LoginEntity, Exception>> getToken(
-      LoginEntity params);
-  Future<Either<LoginEntity, Exception>>
-      resgisterUser(LoginEntity params);
+  Future<Either<LoginEntity, Exception>> getToken(LoginEntity params);
+  Future<Either<LoginEntity, Exception>> resgisterUser(LoginEntity params);
+  Future<Either<LoginEntity, Exception>> saveUser(LoginEntity params);
 }
