e471814ade433ace3d0209ca96832106dea65493
joaoffnogueira <contact@joaonogueira.dev>
2023-12-17 15:20:06 -0300

    split repository

--- list/lib/src/modules/student/domain/repositories/student_remote_repository.dart
+++ list/lib/src/modules/student/domain/repositories/student_remote_repository.dart
@@ -1,15 +1,10 @@
 import 'package:dartz/dartz.dart';
 import '../entities/student_entity.dart';
 
-abstract class StudentRepository {
+abstract class StudentRemoteRepository {
   Future<Either<void, Exception>> deleteStudent(String params);
-  Future<Either<void, Exception>> editStudent(StudentEntity params);
-  Future<Either<void, Exception>> eraseStudent(String params);
   Future<Either<List<StudentEntity>, Exception>> fetchStudents();
   Future<Either<StudentEntity, Exception>> getStudent(String params);
-  Future<Either<StudentEntity, Exception>> loadStudent(String params);
   Future<Either<StudentEntity, Exception>> postStudent(StudentEntity params);
   Future<Either<StudentEntity, Exception>> putStudent(StudentEntity params);
-  Future<Either<void, Exception>> saveStudent(StudentEntity params);
-  Future<Either<void, Exception>> saveStudents(List<StudentEntity> params);
 }
--- list/lib/src/modules/student/domain/usecases/fetch_students_use_case.dart
+++ list/lib/src/modules/student/domain/usecases/fetch_students_use_case.dart
@@ -1,13 +1,13 @@
 import 'package:dartz/dartz.dart';
 import '../../../../utils/usecase.dart';
 import '../entities/student_entity.dart';
-import '../repositories/student_repository.dart';
+import '../repositories/student_remote_repository.dart';
 
 class FetchStudentsUseCase
     extends UseCase<List<StudentEntity>> {
   FetchStudentsUseCase(this.repository);
 
-  final StudentRepository repository;
+  final StudentRemoteRepository repository;
 
   @override
   Future<Either<List<StudentEntity>, Exception>> execute() async {
