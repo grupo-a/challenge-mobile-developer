d70d64a37450dce1ab9781949214f5cf6ac7e8f4
joaoffnogueira <contact@joaonogueira.dev>
2023-12-18 09:06:03 -0300

    make sure db is initialized

--- list/lib/src/modules/student/data/datasources/student_local_data_source_impl.dart
+++ list/lib/src/modules/student/data/datasources/student_local_data_source_impl.dart
@@ -1,20 +1,33 @@
 import 'package:core_services/core_services.dart';
+import 'package:lists_joao_nogueira/src/modules/student/data/models/student_model.dart';
 import 'package:lists_joao_nogueira/src/modules/student/domain/entities/student_entity.dart';
 
 import '../../domain/datasources/student_datasource.dart';
 
 class StudentLocalDataSourceImpl implements StudentLocalDataSource {
   final CoreDatabaseInterface<StudentEntity> coreStorage;
+  bool _initialized = false;
 
   StudentLocalDataSourceImpl({required this.coreStorage});
 
+  Future<void> makeSureInitialized() async {
+    if (_initialized) {
+      return Future.value();
+    } else {
+      _initialized = true;
+      await coreStorage.init('students', StudentModelAdapter());
+    }
+  }
+
   @override
   Future<void> eraseStudent(String params) async {
+    await makeSureInitialized();
     await coreStorage.delete(params);
   }
 
   @override
   Future<void> saveStudent(StudentEntity params) async {
+    await makeSureInitialized();
     StudentEntity? student = await coreStorage.get(key: params.id!);
     if (student == null) {
       await coreStorage.save(key: params.id!, value: params);
@@ -26,6 +39,7 @@ class StudentLocalDataSourceImpl implements StudentLocalDataSource {
 
   @override
   Future<void> saveStudents(List<StudentEntity> params) async {
+    await makeSureInitialized();
     var map = {for (var e in params) e.id: e};
     await coreStorage.clear();
     await coreStorage.saveAll(values: map);
@@ -33,6 +47,7 @@ class StudentLocalDataSourceImpl implements StudentLocalDataSource {
 
   @override
   Future<StudentEntity?> loadStudent(String params) async {
+    await makeSureInitialized();
     return await coreStorage.get(key: params);
   }
 }
