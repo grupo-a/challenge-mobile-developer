749f372ef6ce4e9eab2f1cec9c1359e786678ba6
joaoffnogueira <contact@joaonogueira.dev>
2023-12-18 08:23:09 -0300

    remove unused

--- list/core_services/lib/src/models/model_base.dart
+++
@@ -1,8 +0,0 @@
-abstract class ModelBase {
-  int? id;
-  int? usuarioId;
-
-  ModelBase({this.id, this.usuarioId});
-  ModelBase.fromJson(Map<String, dynamic> json);
-  Map<String, dynamic> toJson();
-}
